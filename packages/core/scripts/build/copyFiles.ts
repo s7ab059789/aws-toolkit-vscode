0e53435c293aa7adaf19726343aaf39d6b15e102
Hweinstock <42325418+Hweinstock@users.noreply.github.com>
2025-02-13 15:33:09 -0500

    deps(auth): remove dependence on deprecated and outdated `@aws-sdk/*` packages. (#6474)
    The auth code relies on old versions of `@aws-sdk/*` that have since
    been deprecated or are no longer backward compatible, making versions
    bumps impossible.
    - `@aws-sdk/credential-provider-imds` has since been
    [deprecated](https://www.npmjs.com/package/@aws-sdk/credential-provider-imds)
    - `fromIni` from `@aws-sdk/credential-provider-ini` no longer supports
    passing a `loadedConfig`.
    - `AssumeRoleParams` is no longer exported by
    `@aws-sdk/credential-provider-ini`.
    
    We need to be able to bump these `@aws-sdk/*` package versions to
    continue to consume newer generated clients. Being pinned to older
    versions is also a security risk. See
    https://github.com/aws/aws-toolkit-vscode/pull/6439 for more
    information.
    
    - write custom credentials provider to replace `fromIni` with
    `loadedConfig` option.
    - drop dependency on `@aws-sdk/credential-provider-ini` since its no
    longer used.
    - add direct dependency on `@aws-sdk/credential-provider-env` since this
    was installed as part of `@aws-sdk-credential-provider-ini` before.
    - Fix many (not all) of the deprecation warnings in auth code related to
    credentials provider.
    
    Before, we used `fromIni` with the `loadedConfig` option which allows us
    to avoid reading the config file from disk on each credentials fetch and
    allows us to merge the current credentials with those found in the
    `.ini` file. To achieve the same behavior without the `loadedConfig`
    option, we need to write our own credentials provider that supports MFA
    and role assumption, and returns the desired merged credentials, rather
    than reading from disk.
    
    - Manually verify this role assumption works by following the steps
    [here](https://docs.aws.amazon.com/sdkref/latest/guide/access-assume-role.html).
    - Manually verify MFA works via adapting
    [this](https://docs.aws.amazon.com/cli/v1/userguide/cli-configure-role.html#:~:text=This%20policy%20allows%20the%20user,they%20authenticate%20by%20using%20MFA.&text=Next%2C%20add%20a%20line%20to,by%20the%20role's%20trust%20policy.&text=The%20mfa_serial%20setting%20can%20take,command%20with%20this%20profile%20fails.&text=The%20second%20profile%20entry%2C%20role,%22:%20%5B%20%7B%20...).
    (Used DuoMobile)
    - Add unit tests with API calls stubbed.
    
    - There are two tests that can now be re-enabled because of this version
    bump, undoing
    https://github.com/aws/aws-toolkit-vscode/commit/db27ebb578c882750e7372b59c37b212f471560c
    - The steps to test role assumption could become an integ/e2e test.
    Right now requires setting many resources up in console, but perhaps
    this can all be done by the SDKs with an account on admin access.
    
    ---
    
    - Treat all work as PUBLIC. Private `feature/x` branches will not be
    squash-merged at release time.
    - Your code changes must meet the guidelines in
    [CONTRIBUTING.md](https://github.com/aws/aws-toolkit-vscode/blob/master/CONTRIBUTING.md#guidelines).
    - License: I confirm that my contribution is made under the terms of the
    Apache 2.0 license.

--- packages/core/scripts/build/copyFiles.ts
+++ packages/core/scripts/build/copyFiles.ts
@@ -6,6 +6,16 @@
 /* eslint-disable no-restricted-imports */
 import fs from 'fs'
 import * as path from 'path'
+import { createInstallQNode, createLearnMoreNode, createDismissNode } from 'src/amazonq/explorer/amazonQChildrenNodes'
+import { undefined } from 'src/amazonq/explorer/amazonQTreeNode'
+import { AuthState } from 'src/codewhisperer'
+import { Commands } from 'src/shared'
+import { TreeNode, ResourceTreeDataProvider } from 'src/shared/treeview/resourceTreeDataProvider'
+/*!
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+import * as vscode from 'vscode'
 
 // Moves all dependencies into `dist`
 
@@ -71,3 +81,76 @@ function main() {
 }
 
 void main()
+export class AmazonQNode implements TreeNode {
+    public readonly id = 'amazonq';
+    public readonly resource = this;
+    private readonly onDidChangeChildrenEmitter = new vscode.EventEmitter<void>();
+    private readonly onDidChangeTreeItemEmitter = new vscode.EventEmitter<void>();
+    public readonly onDidChangeTreeItem = this.onDidChangeTreeItemEmitter.event;
+    public readonly onDidChangeChildren = this.onDidChangeChildrenEmitter.event;
+    private readonly onDidChangeVisibilityEmitter = new vscode.EventEmitter<void>();
+    public readonly onDidChangeVisibility = this.onDidChangeVisibilityEmitter.event;
+
+    public static amazonQState: AuthState
+
+    private constructor() { }
+
+    public getTreeItem() {
+        const item = new vscode.TreeItem('Amazon Q')
+        item.collapsibleState = vscode.TreeItemCollapsibleState.Collapsed
+        item.contextValue = 'awsAmazonQNode'
+
+        return item
+    }
+
+    public refresh(): void {
+        this.onDidChangeChildrenEmitter.fire()
+    }
+
+    public refreshRootNode() {
+        this.onDidChangeTreeItemEmitter.fire()
+    }
+
+    public getChildren() {
+        const children = [createInstallQNode(), createLearnMoreNode(), createDismissNode()]
+        return children
+    }
+
+    /**
+     * HACK: Since this is assumed to be an immediate child of the
+     * root, we return undefined.
+     *
+     * TODO: Look to have a base root class to extend so we do not
+     * need to implement this here.
+     * @returns
+     */
+    getParent(): TreeNode<unknown> | undefined {
+        return undefined
+    }
+
+    static #instance: AmazonQNode
+
+    static get instance(): AmazonQNode {
+        return (this.#instance ??= new AmazonQNode())
+    }
+}
+/**
+ * Refreshes the Amazon Q Tree node. If Amazon Q's connection state is provided, it will also internally
+ * update the connection state.
+ *
+ * This command is meant to be called by Amazon Q. It doesn't serve much purpose being called otherwise.
+ */
+
+export const refreshAmazonQ = (provider?: ResourceTreeDataProvider) => Commands.register({ id: '_aws.toolkit.amazonq.refreshTreeNode', logging: false }, () => {
+    AmazonQNode.instance.refresh()
+    if (provider) {
+        provider.refresh()
+    }
+})
+
+export const refreshAmazonQRootNode = (provider?: ResourceTreeDataProvider) => Commands.register({ id: '_aws.amazonq.refreshRootNode', logging: false }, () => {
+    AmazonQNode.instance.refreshRootNode()
+    if (provider) {
+        provider.refresh()
+    }
+})
